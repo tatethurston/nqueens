41c33e3f9f68e5ae5c38ba3fb5c9b9b7c49a0733
Tate Thurston <Tate Thurston>
2015-10-13 11:41:33 -0700

    updated variable name to rook

--- nRooks.js
+++ nRooks.js
@@ -1,19 +1,19 @@
 var nRooks = function(n){
 
-  var _nRooks = function(queenLocations, all, positions, bit, count){
+  var _nRooks = function(rookLocations, all, positions, bit, count){
     //possible positions are anywhere where col not set
     //bit mask positions to size of n
-    positions = ~(queenLocations) & all;
+    positions = ~(rookLocations) & all;
     
     //while there is a move;
     while(positions > 0){
       //get the least most bit and remove from positions
       positions ^= bit = positions & -positions;
-      //recursive call adding move to queenLocations
-      count += _nRooks(queenLocations | bit, all, 0,0,0);
+      //recursive call adding move to rookLocations
+      count += _nRooks(rookLocations | bit, all, 0,0,0);
     }
     //increment count if num queens equals n
-    queenLocations==all && ++count;
+    rookLocations==all && ++count;
     return count;
   };
 
